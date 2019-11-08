b00f54d42b28e65f9cf10eaf822e28036d521301
Reed James <reedtjames1234@gmail.com>
2019-11-08 13:10:57 -0600

    times up

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,51 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+cache = {}
+
+
 def count_th(word):
-    
+    n = 0
+    amount = 0
+    global cache
     # TBC
-    
-    pass
+    if word == "" or len(word) == 1:
+        print('first base')
+        return 0
+    if word[n] == "t" and word[n+1] == "h":  # word[n] and word[n+1]
+        amount = amount + 1
+        print(f'amount after increment: {amount}')
+    if word[n] not in cache:
+        cache[word[n]] = count_th(word[:n])
+        n = n + 1
+    print(f'letter at index: {word[n]}')
+    print(f'word length: {len(word)}')
+    if n == len(word)-1:
+        return amount
+
+
+'''
+Your function should take in a signle parameter (a string word)
+Your function should return a count of how many occurences of "th" occur within word. Case matters.
+Your function must utilize recursion.
+It cannot contain any loops.
+
+# Understand
+
+-I need to go through the string and look for instances of the lower case letter t
+-I need to check if following letter is an h
+-I need to check for case but that should be easy since their not equal
+
+# Planning
+
+-I need a base case for if the sting is empty
+-I need to declare n = 0 
+-I need to declare an amount = 0
+-I need a base case for checking if base case of n is a t and if it is if the following letter is an h then amount = 1
+-I need an empty dict (cache) to store the 'n' value which will be each 'index' in a string.  for example word[1] should print first letter 
+    -I should have an if statement checking if n is in the cache
+    -it should then be incremented by 1
+        -count_th(word[:n+1])
+        -it should cycle through and hit the base cases at every lowercase t
+'''
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -97,16 +99,187 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        print('test')
+        self.swap_item()
+        print(f'base swap position:item: {self._position} {self._item}')
+        while self._light == "OFF":
+            print('while light off start')
+            print(f'list now: {self._list}')
+
+            print(f'position: {self._position}')
+            while self.can_move_left():
+                print("moving left")
+                self.move_left()
+                print('moved left')
+            self.set_light_on()
+            print(f'light turned on: {self._light}')
+            print(f'position:item: {self._position} {self._item}')
+
+            self.swap_item()
+            print('item picked up')
+            print(f'position:item: {self._position} {self._item}')
+            print(f'list now: {self._list}')
+            while self.can_move_right():
+                print('moving right')
+                self.move_right()
+                if self.compare_item() == 1:
+                    print(f'robot is holdin a larger item')
+                    self.swap_item()
+                    print(f'list now in right if: {self._list}')
+                print('moved right')
+                self.set_light_off()
+            # if self._position == len(self._list - 1) and self._list[-1] == None:
+            #     self.set_light_on
+            # if self.swap_item() == None:
+            #     if self.can_move_right() == False:
+            #         self.swap_item()
+            #         self.set_light_on
+            #     self.move_right()
+            # if self.compare_item() == 1:
+            #     print('comparing')
+            #     self.swap_item()
+            #     print('swapped')
+            #     self.set_light_off()
+            #     print('light on in if')
+            #     print(f'list now: {self._list}')
+            # self.set_light_on()
+            # print(f'Light should be on {self._light}')
+            # elif self.compare_item == -1:
+            #     self.move_left
+            #     self.swap_item
+            # self.set_light_off
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
     robot.sort()
     print(robot._list)
+
+
+'''
+###Understand
+-the robot can move left or right 1 at a time and stops at either end of the list
+-it will swap whatever item it has with whatever is in front of it
+-it can tell if an item is smaller, larger or equal to the item its holding by returning -1,1, or 0
+-The robot has a boolean value in the form of a 'light' that turns on or off
+-it kinda seems like a bubble sort
+
+###Planning
+def bubble_sort(arr):
+    done = True
+    while done:
+        done = False
+        for i in range(0, len(arr)-1):
+            if arr[i] > arr[i+1]:
+                # swap
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                done = True
+    return arr
+
+-Maybe it should all be in a while loop for light off
+    -another while for can_move left true
+        -move left
+        -check if it can move left
+    -turn light on(set_light_on)
+    -It will start at the index of 0 of a list
+    -Then it will pick up the first item (index 0). 
+    -if it can move right it will by index of 1
+        -move_right
+    -it will compare the item its holding to the item in front of it
+        -if the item in front is 1(larger) compare_item == 1 it will swap items 
+            -swap_item
+        -if can_move_right is false then is should drop the item
+    -I will need to end the loop
+        -I can check if none is in the last item of the array
+        -I can set the light on to break the loop
+##################################
+while light_is_on is False:
+    while can_move_left:
+        move_left
+    set_light_on
+    swap_item
+    while can_move_right:
+        move_right
+        if compare_item == 1:
+            swap_item
+        elif compare_item == -1:
+            move_left
+            swap_item
+        set_light_off
+#########################
+if can_move_right not True:
+            swap_item
+            set_light_off
+########################
+
+-Im going to need to be able to drop the item
+
+-its light will be off
+-while set_light_off
+    -set_light_on
+    -for loop for i in range(0, len(arr)-1):
+
+
+    -It will check if it can move left or right
+
+##########################################
+while self.light_is_on is False:
+            print('while light test')
+            while self.can_move_left:
+                self.move_left
+            self.set_light_on
+            print(f'light: {self._light}')
+            self.swap_item
+            while self.can_move_right:
+                self.move_right
+            if self.compare_item == 1:
+                self.swap_item
+            # elif self.compare_item == -1:
+            #     self.move_left
+            #     self.swap_item
+            self.set_light_off
+
+#########################################
+print('test')
+        self.swap_item()
+        print(f'base swap position:item: {self._position} {self._item}')
+        while self._light == "OFF":
+            print('while light off start')
+            print(f'list now: {self._list}')
+            self._position
+            print(f'position: {self._position}')
+            while self.can_move_left():
+                print("moving left")
+                self.move_left()
+                print('moved left')
+            self.set_light_on()
+            print(f'light turned on: {self._light}')
+            print(f'position:item: {self._position} {self._item}')
+            if self.swap_item() == None:
+                if self.can_move_right() == False:
+                    self.swap_item()
+                    self.set_light_on
+                self.move_right()
+            self.swap_item()
+            print('item picked up')
+            print(f'position:item: {self._position} {self._item}')
+            print(f'list now: {self._list}')
+            while self.can_move_right():
+                print('moving right')
+                self.move_right()
+                if self.compare_item() == 1:
+                    print(f'robot is holdin a larger item')
+                    self.swap_item()
+                    print(f'list now in right if: {self._list}')
+                print('moved right')
+                self.set_light_off()
+
+
+'''
